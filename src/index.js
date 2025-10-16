819798bcb6a4e306622341756fea360fcc8afb7e
Александр Васянькин <henchfiend@yandex.ru>
2025-10-17 02:04:54 +0300

    Адаптивная вёрстка ч9
    - footer приведён к макету
    - header приведён к макету
    - кнопки слайдера теперь всегда на одинаковом расстоянии от края контента
    - все стили вынесены в отдельные файлы
    - Картинки продуктов на разрешении больше 1024px теперь отображаются корректно
    - Изменен метод подключения видео

--- src/index.js
+++ src/index.js
@@ -17,9 +17,11 @@ const playButton = document.getElementById("about-video-play");
 const videoPreviewContainer = document.getElementById(
   "about-video-preview-container"
 );
-const videoWrap = document.getElementById("about-video-wrap");
+// const videoWrap = document.getElementById("about-video-wrap");
+const iframe = document.getElementById("video-iframe");
 
 playButton.addEventListener('click', () => {
     videoPreviewContainer.classList.add("about__video-preview-container_display_none");
-    videoWrap.innerHTML = "<iframe src='https://vk.com/video_ext.php?oid=-219828625&id=456239022&autoplay=1' allow='autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;' frameborder='0' allowfullscreen class='about__video'></iframe>"
+    iframe.src =
+      "https://vk.com/video_ext.php?oid=-219828625&id=456239022&autoplay=1";
 })
